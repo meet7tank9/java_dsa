541e8c0de0a0c0b23232458d4d2df646800180ec
meet7tank9 <83707307+meet7tank9@users.noreply.github.com>
2022-11-08 16:28:29 +0530

    Add files via upload

---
+++ MountainArray1.java
@@ -0,0 +1,28 @@
+//find the largest number in the mountain array
+public class MountainArray1 {
+    public static void main(String[] args) {
+        int[] arr={1,4,6,79,89,6,5,1};
+
+        System.out.println(binarySearch(arr));
+
+    }
+    static int binarySearch(int[] arr){
+        int start=0;
+        int end=arr.length-1;
+
+        while(start<end){
+            int mid=(start+end)/2;
+
+            if(arr[mid]>arr[mid+1]){
+                //means you are in descending part of the array
+                //means you have to check left side elements of the array
+                end=mid;
+            }
+            else{
+                //otherwise you have to check right side elements of the array
+                start=mid+1;
+            }
+        }
+        return start;//or you can also return end, both will be same at the end;
+    }
+}
